abef20599dbfc3cb694d98c5d81ce681df8ee118
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-05-04 06:52:35 +0000

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@ Admin.create!(
   email: 'admin@admin',
   password: '000000'
   )
+
+Store.create!(
+  email: 'tokyo@tokyo',
+  name: '東京タワー店',
+  postal_code: '1050011',
+  phone_number: '11111111111',
+  prefecture_code: '東京都',
+  address_city: '市町村',
+  address_street: '番地',
+  address_building: '建物名',
+  password: '111111'
+  )
