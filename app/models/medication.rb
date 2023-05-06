4028a7ceed4974d143f1d567016cb2b55f375151
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-05-06 03:30:41 +0000

    投薬投稿画面更新

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -7,12 +7,13 @@ class Medication < ApplicationRecord
   belongs_to :individual
   belongs_to :store
   has_many :dairies, dependent: :destroy
-  
+
   #投薬のステータス
   enum medication_status: { give: true, not_give: false }
-  
+  enum dosing_times: { sid: 0, bid: 1, tid: 3 }
+
   #投薬計算
-  
-  
+
+
 
 end
