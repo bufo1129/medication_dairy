f4b820f9ded64afcc805e0aad0406b26105f6725
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-05-11 03:38:59 +0000

    店舗側個体一覧レイアウト修正

--- app/controllers/store/medicines_controller.rb
+++
@@ -1,9 +0,0 @@
-class Store::MedicinesController < ApplicationController
-  before_action :authenticate_store!
-
-  def show
-    @medicine = Medicine.find(params[:id])
-    @medicines = Medicine.all
-  end
-
-end
