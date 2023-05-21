04b767d2a21298fd5e2a12ef1198346f8689396e
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-05-21 11:30:34 +0000

    管理者ページ修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,11 +35,11 @@ $(function() {
 });
 
 // アコーディオン
-  $(function(){
-    $('.js-accordion-title').on('click', function () {
-      /*クリックでコンテンツを開閉*/
-      $(this).next().slideToggle(200);
-      /*矢印の向きを変更*/
-      $(this).toggleClass('open', 200);
-    });
+$(function(){
+  $('.js-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 200);
   });
+});
