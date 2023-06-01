5fbd4790afaf726e11543a2118a40a5c486dd9d1
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-06-01 03:49:45 +0000

    モデルにコメント追加

--- app/models/dairy.rb
+++ app/models/dairy.rb
@@ -4,11 +4,10 @@ class Dairy < ApplicationRecord
   belongs_to :medication, optional: true
   belongs_to :weather
 
-  # ↓日報の登録日順の並べ替え
+  # 日報の登録日順の並べ替え
   scope :latest, -> {order(created_date: :desc)}
   scope :old, -> {order(created_date: :asc)}
 
-  #バリデーション
   validates :weather_id,        presence: true
   validates :high_temperature,  presence: true
   validates :low_temperature,   presence: true
--- app/models/see.rb
+++ app/models/see.rb
@@ -1,4 +1,5 @@
 class See < ApplicationRecord
+  # 投薬記録コメントのいいねモデル
   belongs_to :comment
   belongs_to :store
   validates_uniqueness_of :comment_id, scope: :store_id
