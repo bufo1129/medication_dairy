f49be766b34493aabe6949b267e0bc55e49a3758
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-06-14 11:42:53 +0000

    seedのsystem、Rspec修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
