70b3c22005abd07f58f1555a23840901ba3a9df2
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-06-20 11:12:34 +0000

    住所自動入力機能修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
-import './address_autofill.js'
 import './dosageCalculation'
 // import './script.js'
 // import 'jquery-3.6.4.min.js'
